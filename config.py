46feb6aaf6f394c2a3ee32b6e6a11155e0946ea4
Aman Mishra <am30435@gmail.com>
2026-01-02 13:41:55 +0530

    Refactor config parameters to allow None values and add batch_size; update notebook for W&B integration and logging; modify requirements to ensure wandb is included.

--- config.py
+++ config.py
@@ -3,11 +3,12 @@ from dataclasses import dataclass
 
 @dataclass
 class Parameters:
-    device : str
-    epochs : str
-    learning_rate : float
-    d_model : int
-    h : int
-    blocks : int
-    vocab_size : int
-    dropout : float
+    device : str | None = None
+    epochs : int | None = None
+    learning_rate : float | None = None
+    d_model : int | None = None
+    h : int | None = None
+    blocks : int | None = None
+    vocab_size : int | None = None
+    dropout : float | None = None
+    batch_size : int | None = None
